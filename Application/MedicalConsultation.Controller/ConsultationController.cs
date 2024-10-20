e7d775d4a38ce19200c694e0a4d83081a91ad8a6
Ricardo Moreira <ricardo.moreira@g.globo>
2024-10-20 16:58:21 -0300

    firs commit

---
+++ Application/MedicalConsultation.Controller/ConsultationController.cs
@@ -0,0 +1,72 @@
+﻿using MedicalConsultation.Entity.Consultation;
+using MedicalConsultation.Interfaces.Controller;
+using MedicalConsultation.Interfaces.Gateway;
+using MedicalConsultation.UseCases.Consultation;
+using MedicalConsultation.UseCases.Patient;
+
+namespace MedicalConsultation.Controller
+{
+    public class ConsultationController : IConsultationController
+    {
+        private readonly IConsultationGateway _consultationGateway;
+        private readonly IPatientGateway _patientGateway;
+        private readonly IMedicalDoctorGateway _medicalDoctorGateway;
+
+        public ConsultationController(IConsultationGateway consultationGateway, IPatientGateway patientGateway, IMedicalDoctorGateway medicalDoctorGateway)
+        {
+            _consultationGateway = consultationGateway;
+            _patientGateway = patientGateway;
+            _medicalDoctorGateway = medicalDoctorGateway;
+        }
+
+        public ConsultationEntity Alterar(ConsultationEntity entity)
+        {
+            return _consultationGateway.AtualizarConsulta(entity);
+        }
+
+        public ConsultationEntity Incluir(ConsultationEntity entity)
+        {
+            var paciente = _patientGateway.ObterPorId(entity.PacienteId);
+            var medico = _medicalDoctorGateway.ObterPorId(entity.MedicoId);
+
+            DateTime datahoraInicio = entity.Date;
+            DateTime datahoraFim = entity.Date.AddMinutes(30);
+
+            var consultasPaciente = _consultationGateway.ObterConsultasPacienteNoPeriodo(entity.PacienteId, datahoraInicio, datahoraFim);
+            var consultasMedico = _consultationGateway.ObterConsultasMedicoNoPeriodo(entity.MedicoId, datahoraInicio, datahoraFim);
+
+            var useCase = new IncludeConsultationUseCase(
+                medico, 
+                paciente, 
+                consultasPaciente?.FirstOrDefault(), 
+                consultasMedico?.FirstOrDefault(), 
+                entity);
+
+            var obj = useCase.VerificarExistente();
+
+            _consultationGateway.IncluirConsulta(obj);
+
+            return obj;
+        }
+
+        public IEnumerable<ConsultationEntity> ListarPorMedico(int id)
+        {
+            return _consultationGateway.ListarConsultasMedicoAPartirDe(id, DateTime.Now)?.ToList();
+        }
+
+        public IEnumerable<ConsultationEntity> ListarPorMedicoAPartirDe(int id, DateTime data)
+        {
+            return _consultationGateway.ListarConsultasMedicoAPartirDe(id, data)?.ToList();
+        }
+
+        public IEnumerable<ConsultationEntity> ListarPorPaciente(int id)
+        {
+            return _consultationGateway.ListarConsultasPacienteAPartirDe(id, DateTime.Now).ToList();
+        }
+
+        public IEnumerable<ConsultationEntity> ListarPorPacienteAPartirDe(int id, DateTime data)
+        {
+            return _consultationGateway.ListarConsultasPacienteAPartirDe(id, data).ToList();
+        }
+    }
+}
---
+++ Application/MedicalConsultation.Controller/MedicalDoctorController.cs
@@ -0,0 +1,91 @@
+﻿using MedicalConsultation.Entity.MedicalDoctor;
+using MedicalConsultation.Interfaces.Controller;
+using MedicalConsultation.Interfaces.Gateway;
+using MedicalConsultation.UseCases.MedicalDoctor;
+
+namespace MedicalConsultation.Controller
+{
+    public class MedicalDoctorController : IMedicalDoctorController
+    {
+        private readonly IMedicalDoctorGateway _doctorGateway;
+        private readonly IConsultationGateway _consultationGateway;
+        private readonly INotificationGateway _notificationsGateway;
+
+        public MedicalDoctorController(IMedicalDoctorGateway doctorGateway,
+            IConsultationGateway consultationGateway,
+            INotificationGateway notificationsGateway)
+        {
+            _doctorGateway = doctorGateway;
+            _consultationGateway = consultationGateway;
+            _notificationsGateway = notificationsGateway;
+        }
+
+        public MedicalDoctorEntity Alterar(MedicalDoctorEntity entity)
+        { 
+            var registroBase = _doctorGateway.ObterPorId(entity.Id);
+            var registroPorEmail = _doctorGateway.ObterPorEmail(entity.Email);
+            var useCase = new UpdateMedicalDoctorUseCase(entity, registroBase, registroPorEmail);
+
+            useCase.VerificarExistente();
+
+            registroBase.SetEmail(entity.Email);
+            registroBase.SetCrm(entity.CRM);
+            registroBase.SetName(entity.Name);
+            registroBase.SetEspecialidade(entity.Especialidade);
+
+            return _doctorGateway.AtualizarMedico(entity);
+        }
+
+        public MedicalDoctorEntity Incluir(MedicalDoctorEntity entity)
+        {
+            var registroBase = _doctorGateway.ObterPorEmail(entity.Email);
+            var useCase = new IncludeMedicalDoctorUseCase(entity, registroBase);
+
+            var obj = useCase.VerificarExistente();
+            
+            if (obj.Id == 0)
+                _doctorGateway.RegistrarMedico(obj);
+            else
+                _doctorGateway.AtualizarMedico(obj);//reativação do usuário
+
+            return entity;
+        }
+
+        public IEnumerable<MedicalDoctorEntity> ListarAtivos()
+        {
+            return _doctorGateway.ObterAtivos();
+        }
+
+        public MedicalDoctorEntity ObterUsuarioPorEmail(string email)
+        {
+            return _doctorGateway.ObterPorEmail(email);
+        }
+
+        public MedicalDoctorEntity ObterUsuarioPorId(int id)
+        {
+            return _doctorGateway.ObterPorId(id);
+        }
+        public bool Excluir(int id)
+        {
+            var entity = _doctorGateway.ObterPorId(id);
+            entity.SetAtivo(false);
+
+            //atualizar cancelar consultas
+            var consultasMedico = _consultationGateway.ListarConsultasMedicoAPartirDe(id, DateTime.Now)?.ToList();
+            consultasMedico?.ForEach(c =>
+            {
+                if (c.Status != Entity.ConsultationStatus.Cancelada)
+                {
+                    c.SetStatus(Entity.ConsultationStatus.Cancelada);
+                    _consultationGateway.AtualizarConsulta(c);
+                    //enviar notificação
+                    _notificationsGateway.SendNotification(
+                        new Entity.Notify.ConsultationNotificationEntity(c, DateTime.Now));
+                }
+            });
+
+            return _doctorGateway.AtualizarMedico(entity) != null;
+        }
+
+    }
+}
